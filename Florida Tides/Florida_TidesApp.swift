d8237a086a600649a7801d1ae315af5aa0af16fe
Barry Hayes <bhayes16@live.spcollege.edu>
2025-10-08 00:51:33 -0400

    Initial Commit

---
+++ Florida Tides/Florida_TidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Florida_TidesApp.swift
+//  Florida Tides
+//
+//  Created by Barry Hayes on 10/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Florida_TidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Florida TidesTests/Florida_TidesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Florida_TidesTests.swift
+//  Florida TidesTests
+//
+//  Created by Barry Hayes on 10/8/25.
+//
+
+import Testing
+@testable import Florida_Tides
+
+struct Florida_TidesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
